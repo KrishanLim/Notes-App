e468f4758a6f108004bd1b816eed0b9f654da6bb
Krishan Limbu <nikitlimbu006@gmail.com>
2026-01-17 00:24:29 -0500

    removed objects from init declaration

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 """
 Notes App where User can login and store their notes and mark them as done or not done
 """
+import base64               #Used for password
 
 class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self):
+        self.username = ''
+        self.password = ''
         self.done = False
-        self.notes = [{
-            'username': username,
-            'password': password,
+        self.notes = [{                     #Dict stores username password notes and done or not
+            'username': self.username,
+            'password': self.password,
             'notes': [
                 {'Content':'' , 'Done': 'False'}
             ]
         }]
 
     def new_user(self):
-        print("New User")
+        print("New User \n")
+        self.username = input("Enter your username: ")
+        self.password = input("Enter your password: ")
 
-            username = input("Enter your username: ")
-            password = input("Enter your password: ")
+
+
+
