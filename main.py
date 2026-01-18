1172059919c4dd83bafdc958c8ae4c3842062dae
Krishan Limbu <nikitlimbu006@gmail.com>
2026-01-17 19:17:12 -0500

    Added loop to check if new user already exists

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 """
 Notes App where User can login and store their notes and mark them as done or not done
 """
-import base64               #Used for password
+from getpass import getpass               #Used for password
 
 class User:
     def __init__(self):
         self.done = False
-        self.name = {                     #Dict stores username password notes and done or not
+        self.name = {                     #Dict stores username password and notes
             'username': '',
             'password': '',
             'notes': []
@@ -16,12 +16,25 @@ class User:
     def new_user(self):             #Func to add new user
         print("New User \n")
         self.name['username'] =input("Enter your username: ")
-        self.name['password'] =input("Create your password: ")
-        self.users.append(self.name)
+        self.name['password'] =getpass("Create your password: ")
+        for user in self.users:                    #Checks if user already exists
+            if self.name==user:
+                print("User already exists")
+                self.done=True
+                return 
+        self.users.append(self.name)    #appends new user to users list
+        return self.users
+    
+    def existing_user(self):              #Func for existing user login
+        print("Existing User \n")
+        username =input("Enter your username: ")
+        password =getpass("Enter your password: ")
+    
 
 
 user=User()
 user.new_user()
+print(user.users)
 
 
 
